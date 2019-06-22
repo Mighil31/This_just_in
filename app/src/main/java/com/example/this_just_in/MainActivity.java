364b1258cb5e5fefb310a1b4e130875cfa0293a9
mighil <mighildath@gmail.com>
2019-06-22 20:34:55 +0000

    Create buttons and activity for news items
    Created two new news categories "trending" and "India". The main activity contains the buttons
    for "trending" and "india" which whenpressed open their respective activities.

--- app/src/main/java/com/example/this_just_in/MainActivity.java
+++ app/src/main/java/com/example/this_just_in/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.this_just_in;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView trending = findViewById(R.id.trending);
+        TextView india = findViewById(R.id.india);
+
+        trending.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the numbers View is clicked on.
+            @Override
+            public void onClick(View view) {
+                Intent trendingIntent = new Intent(MainActivity.this, TrendingActivity.class);
+                startActivity(trendingIntent);
+            }
+        });
+
+        india.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent indiaIntent = new Intent(MainActivity.this, IndiaActivity.class);
+                startActivity(indiaIntent);
+
+            }
+        });
+
+
     }
+
+
 }
