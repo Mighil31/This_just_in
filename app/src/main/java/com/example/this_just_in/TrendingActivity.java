3385c786ef90e3cf6c2fd8826672ad9f6236dcd7
mighil <mighildath@gmail.com>
2019-07-15 20:27:45 +0000

    Create IndiaFragment

--- app/src/main/java/com/example/this_just_in/TrendingActivity.java
+++ app/src/main/java/com/example/this_just_in/TrendingActivity.java
@@ -1,27 +1,9 @@
 package com.example.this_just_in;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Context;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ListView;
-import android.widget.ProgressBar;
-import com.nostra13.universalimageloader.core.DisplayImageOptions;
-import com.nostra13.universalimageloader.core.ImageLoader;
-import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
-import java.util.ArrayList;
-import java.util.List;
-
-import java.util.List;
 
 public class TrendingActivity extends AppCompatActivity {
-
-    private NewsAdapter mAdapter;
-    ProgressBar spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,14 +14,7 @@ public class TrendingActivity extends AppCompatActivity {
                 .commit();
 
 
-
-
     }
-
-
-
-
-
 
 
 }
